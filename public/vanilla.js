cd927cf5fca1d1de58e03f8eb50e1daaedd3dabb
daishi <daishi@axlight.com>
2016-10-11 07:33:50 +0900

    query example in vanilla js

--- public/vanilla.js
+++ public/vanilla.js
@@ -1,7 +1,23 @@
-/* global apolloClient */
+/* global apolloClient, graphqlTag */
+/* eslint-env browser */
 
 const ApolloClient = apolloClient.default;
+const createNetworkInterface = apolloClient.createNetworkInterface;
+const gql = graphqlTag.default;
 
+const networkInterface = createNetworkInterface('/graphql');
 const client = new ApolloClient({
+  networkInterface,
 });
-console.log(client);
+
+const query = gql`{
+  posts {
+    name
+    title
+    content
+  }
+}`;
+
+client.query({ query }).then(({ data }) => {
+  document.getElementById('result').innerHTML = JSON.stringify(data.posts, null, 2);
+});
